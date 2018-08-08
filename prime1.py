7f8626c876138a390c4b9b70960ed9e25a4e7890
MadhaviLavu <41770333+MadhaviLavu@users.noreply.github.com>
2018-08-08 16:42:36 +0530

    Update prime1.py

--- prime1.py
+++ prime1.py
@@ -1,11 +1,12 @@
 a,b=input().split()
 a,b=int(a),int(b)
 for i in range(a+1,b):
-    count=0
+    c=0
     for j in range(1,i+1):
         if(i%j==0):
-            count+=1
+            c+=1
     else:
-        if(count==2):
+        if(c==2):
             print(i,end=' ')
+
 
